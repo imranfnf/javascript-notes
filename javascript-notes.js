3d3a5046c6f8fe0c16c1141f10ecaf7c56a74196
imranfnf <imran.bd.net@gmail.com>
2021-12-14 22:00:33 +0600

    essege

---
+++ javascript-notes.js
@@ -0,0 +1,78 @@
+// // 1. length method:: It is used to count the number of characters in a string 
+// // Example:
+
+// // const len = "Hello Javascript";
+// // const result = len.length;
+// // console.log(result);
+// // result = 16;
+
+
+// //2. toLocaleLowerCase() method :: The javascript toLocaleLowerCase() method is used to
+// //changed the string into Lower Case.
+
+// //Example:
+
+// // const str = "Hello Javascript"
+// // const result = str.toLocaleLowerCase();
+// // console.log(result);
+// // //result = "hello javascript"
+
+// //3. indexOf() method:: The indexOf() method returns the first position of a specified
+// // value in a string.
+
+// //Example:
+
+// // const txt = "Lets find where 'pen' occurs!";
+// // const result = txt.indexOf("pen");
+// // console.log(result)
+
+// //result = 17
+
+// //4. slice() method:: The slice method removes the part of a string and return 
+// // the extracted parts of a new string.
+
+// //Example:
+
+// // const txt = "Hello Javascript"
+// // const result = txt.slice(0, 5);
+// // console.log(result)
+// // // result = Hello
+
+// //5. replace() method:: The javascript replace method changes the defined value
+// // to another value
+
+// //Example:
+
+// // const txt = "Hello Javascript"
+// // const result = txt.replace("Javascript", "Imran");
+// // console.log(result)
+//  //result = Hello Imran
+ 
+
+//  6. includes method() :: The includes() method is used to determine whether 
+//  a string contains the characters of a specified string or not. If it exists
+//  then return true if does not exist returns false.
+
+//  Example:
+
+//  console.log(result)
+//  result = true
+
+
+7. concat() method: The javascript concat method is used for joining two or 
+more strings
+
+Example:
+
+const txt1 = "Hello Javascript"
+const txt2 = " How are you"
+const result = txt1.concat(txt2);
+console.log(result)
+result = Hello Javascript How are you
+const txt = "Hello Javascript"
+ const result = txt.includes("Hello")
+ 
+
+
+
+
